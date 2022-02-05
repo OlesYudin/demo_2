6059c76d30cc2ddac37852cf6aea4485745cd388
OlesYudin <oles.yudin2001@gmail.com>
2022-02-05 16:06:37 +0200

    Delete useless ec2

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -13,21 +13,3 @@ resource "aws_instance" "bastion" {
     Environment = var.env
   }
 }
-
-
-# resource "aws_instance" "webserver" {
-#   count                  = length(var.private_subnet_id)         # count numbers of private subnets
-#   ami                    = data.aws_ami.ubuntu.id                # ubuntu 16.04
-#   instance_type          = var.instance_type                     # instant params
-#   subnet_id              = var.private_subnet_id[count.index]    # attach EC2 to subnet
-#   vpc_security_group_ids = var.sg_app.*.id                       # attach sec group
-#   key_name               = var.ssh_key                           # key for SSH connection
-#   user_data              = file("./modules/ec2/shell/apache.sh") # install apache
-
-#   tags = {
-#     Name        = "Webserver-${count.index + 1}"
-#     AZ          = "${var.availability_zone.names[count.index]}"
-#     Owner       = "Student"
-#     Environment = var.env
-#   }
-# }
